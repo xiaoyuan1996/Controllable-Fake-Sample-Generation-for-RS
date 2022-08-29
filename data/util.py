310811a72061c72fa4aaabe2eba7ce0f1f36f0a1
niuwh <niuwh@aircas.cn.com>
2022-08-29 09:59:55 +0800

    update12

--- data/util.py
+++ data/util.py
@@ -59,7 +59,7 @@ def image_process(path,out_path):
         H = H//2
         W = W//2
         #print(H,W)
-    img.resize((H,W,3),Image.ANTIALIAS)
+    img.resize((H,W),Image.ANTIALIAS)
     print(np.shape(img))
     img.save(out_path)
 
