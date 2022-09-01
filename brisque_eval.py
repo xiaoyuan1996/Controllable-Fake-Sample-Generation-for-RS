039beacf139c33508d1933250fb28514e5c35f76
niuwh <niuwh@aircas.cn.com>
2022-09-01 16:50:51 +0800

    update12

---
+++ brisque_eval.py
@@ -0,0 +1,26 @@
+import os
+from brisque import BRISQUE
+
+def eval_brisque(path, status):
+    files = os.listdir(path)
+
+    all_brisque = 0
+    idx = 0
+    for i, f in enumerate(files):
+        if status != None and status not in f:
+            continue
+
+        sub_path = os.path.join(path, f)
+        brisque = BRISQUE(sub_path, url=False).score()
+        all_brisque += brisque
+        idx += 1
+        print("{}/{}, {}:{}".format(i, len(files), sub_path, brisque))
+    return all_brisque / (idx + 1)
+
+
+
+if __name__ == "__main__":
+    path = "/data/diffusion_data/infer/infer_128/sr_save/"
+    status = '.'
+    brisque = eval_brisque(path, status)
+    print("Ave: {}".format(brisque))
