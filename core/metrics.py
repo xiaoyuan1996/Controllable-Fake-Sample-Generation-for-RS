fe1085557c23a767d7ecea7b42df82e16f087058
niuwh <niuwh@aircas.cn.com>
2022-09-02 19:19:21 +0800

    update12

--- core/metrics.py
+++ core/metrics.py
@@ -94,17 +94,18 @@ def calculate_IS(img):
     preds = np.zeros((1, 1000))
     print(np.shape(data), batch_size_i)
     preds[0:1] = get_pred(data)
-    print(preds)
+    #print(preds)
     scores =[]
     for k in range(1):
         part = preds[k: (k + 1), :]  # split the whole data into several parts
-        print(part)
+        #print(part)
         py = np.mean(part, axis=0)  # marginal probability
         scores = []
         for i in range(part.shape[0]):
             pyx = part[i, :]  # conditional probability
             scores.append(entropy(pyx, py))  # compute divergence
     mean = np.exp(np.mean(scores))
+    return mean
 
 
 def ssim(img1, img2):
