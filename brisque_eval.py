221f374375de782dc9d95c2a627869b217fee8cf
niuwh <niuwh@aircas.cn.com>
2022-09-06 10:27:58 +0800

    update12

--- brisque_eval.py
+++ brisque_eval.py
@@ -20,7 +20,7 @@ def eval_brisque(path, status):
 
 
 if __name__ == "__main__":
-    path = "/data/diffusion_data/infer/infer_128_220901_030446/results/hr_save/"
+    path = "/data/diffusion_data/infer/128_test_256_220905_104604/results/sr_save/"
     status = 'sr.'
     brisque = eval_brisque(path, status)
     print("Ave: {}".format(brisque))
