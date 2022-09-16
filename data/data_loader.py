938a8efba091b824fc00928d2e3e496073d02ec4
niuwh <niuwh@aircas.cn.com>
2022-09-16 11:22:20 +0800

    update12

--- data/data_loader.py
+++ data/data_loader.py
@@ -3,7 +3,7 @@ from itertools import chain
 import os
 import random
 
-from munch import Munch
+#from munch import Munch
 from PIL import Image
 import numpy as np
 
@@ -154,47 +154,47 @@ def get_test_loader(root, img_size=256, batch_size=32,
                            pin_memory=True)
 
 
-class InputFetcher:
-    def __init__(self, loader, loader_ref=None, latent_dim=16, mode=''):
-        self.loader = loader
-        self.loader_ref = loader_ref
-        self.latent_dim = latent_dim
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.mode = mode
-
-    def _fetch_inputs(self):
-        try:
-            x, y = next(self.iter)
-        except (AttributeError, StopIteration):
-            self.iter = iter(self.loader)
-            x, y = next(self.iter)
-        return x, y
-
-    def _fetch_refs(self):
-        try:
-            x, x2, y = next(self.iter_ref)
-        except (AttributeError, StopIteration):
-            self.iter_ref = iter(self.loader_ref)
-            x, x2, y = next(self.iter_ref)
-        return x, x2, y
-
-    def __next__(self):
-        x, y = self._fetch_inputs()
-        if self.mode == 'train':
-            x_ref, x_ref2, y_ref = self._fetch_refs()
-            z_trg = torch.randn(x.size(0), self.latent_dim)
-            z_trg2 = torch.randn(x.size(0), self.latent_dim)
-            inputs = Munch(x_src=x, y_src=y, y_ref=y_ref,
-                           x_ref=x_ref, x_ref2=x_ref2,
-                           z_trg=z_trg, z_trg2=z_trg2)
-        elif self.mode == 'val':
-            x_ref, y_ref = self._fetch_inputs()
-            inputs = Munch(x_src=x, y_src=y,
-                           x_ref=x_ref, y_ref=y_ref)
-        elif self.mode == 'test':
-            inputs = Munch(x=x, y=y)
-        else:
-            raise NotImplementedError
-
-        return Munch({k: v.to(self.device)
-                      for k, v in inputs.items()})
+# class InputFetcher:
+#     def __init__(self, loader, loader_ref=None, latent_dim=16, mode=''):
+#         self.loader = loader
+#         self.loader_ref = loader_ref
+#         self.latent_dim = latent_dim
+#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         self.mode = mode
+#
+#     def _fetch_inputs(self):
+#         try:
+#             x, y = next(self.iter)
+#         except (AttributeError, StopIteration):
+#             self.iter = iter(self.loader)
+#             x, y = next(self.iter)
+#         return x, y
+#
+#     def _fetch_refs(self):
+#         try:
+#             x, x2, y = next(self.iter_ref)
+#         except (AttributeError, StopIteration):
+#             self.iter_ref = iter(self.loader_ref)
+#             x, x2, y = next(self.iter_ref)
+#         return x, x2, y
+#
+#     def __next__(self):
+#         x, y = self._fetch_inputs()
+#         if self.mode == 'train':
+#             x_ref, x_ref2, y_ref = self._fetch_refs()
+#             z_trg = torch.randn(x.size(0), self.latent_dim)
+#             z_trg2 = torch.randn(x.size(0), self.latent_dim)
+#             inputs = Munch(x_src=x, y_src=y, y_ref=y_ref,
+#                            x_ref=x_ref, x_ref2=x_ref2,
+#                            z_trg=z_trg, z_trg2=z_trg2)
+#         elif self.mode == 'val':
+#             x_ref, y_ref = self._fetch_inputs()
+#             inputs = Munch(x_src=x, y_src=y,
+#                            x_ref=x_ref, y_ref=y_ref)
+#         elif self.mode == 'test':
+#             inputs = Munch(x=x, y=y)
+#         else:
+#             raise NotImplementedError
+#
+#         return Munch({k: v.to(self.device)
+#                       for k, v in inputs.items()})
