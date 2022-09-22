0eeb61943d47af4cd5acf3b7c16b20e3b9ec1043
niuwh <niuwh@aircas.cn.com>
2022-09-22 14:44:22 +0800

    update12

--- scl_sr.py
+++ scl_sr.py
@@ -123,16 +123,17 @@ if __name__ == "__main__":
                         Metrics.save_img(
                             hr_img, '{}/{}_{}_hr.png'.format(result_path, current_step, idx))
                         Metrics.save_img(
-                            sr_img, '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
+                            sr_img, '{}/{}_{}_sr_process.png'.format(result_path, current_step, idx))
                         Metrics.save_img(
-                            lr_img, '{}/{}_{}_lr.png'.format(result_path, current_step, idx))
+                            Metrics.tensor2img(visuals['SR'][-1]),
+                            '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
                         Metrics.save_img(
                             fake_img, '{}/{}_{}_inf.png'.format(result_path, current_step, idx))
-                        tb_logger.add_image(
-                            'Iter_{}'.format(current_step),
-                            np.transpose(np.concatenate(
-                                (fake_img, sr_img, hr_img), axis=1), [2, 0, 1]),
-                            idx)
+                        # tb_logger.add_image(
+                        #     'Iter_{}'.format(current_step),
+                        #     np.transpose(np.concatenate(
+                        #         (fake_img, sr_img, hr_img), axis=1), [2, 0, 1]),
+                        #     idx)
                         avg_psnr += Metrics.calculate_psnr(
                             sr_img, hr_img)
 
