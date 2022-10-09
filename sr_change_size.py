28858915028660d624648165385ce0f3fe8ad8ec
niuwh <niuwh@aircas.cn.com>
2022-10-09 19:40:46 +0800

    update12

--- sr_change_size.py
+++ sr_change_size.py
@@ -49,7 +49,7 @@ class AverageMeter(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default='config/multiple_256.json',
+    parser.add_argument('-c', '--config', type=str, default='config/crop_generate.json',
                         help='JSON file for configuration')
     parser.add_argument('-p', '--phase', type=str, choices=['train', 'val'],
                         help='Run either train(training) or val(generation)', default='train')
