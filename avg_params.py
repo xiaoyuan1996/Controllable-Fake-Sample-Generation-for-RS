e12411b2dc640bcc364d06d30cb1057a5f62a31c
niuwh <niuwh@aircas.cn.com>
2022-10-21 18:18:00 +0800

    update12

---
+++ avg_params.py
@@ -0,0 +1,31 @@
+import torch
+import argparse
+import os
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-path1', '--path1', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1000000_E2124_gen.pth")
+    parser.add_argument('-path2', '--path2', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1010000_E2145_gen.pth")
+    parser.add_argument('-path3', '--path3', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1020000_E2166_gen.pth")
+    parser.add_argument('-save_dir', '--save_dir', type=str, default="diffusion_data/save_data/large_scale")
+    args = parser.parse_args()
+    new_state ={'epoch': 1, 'iter': 1000000,
+                     'scheduler': None, 'optimizer': None}
+    if args.path1 is not None:
+        opt1 = torch.load(args.path1)
+        state_dict1 = opt1['optimizer']
+    if args.path2 is not None:
+        opt2 = torch.load(args.path2)
+        state_dict2 = opt1['optimizer']
+    if args.path3 is not None:
+        opt3 = torch.load(args.path3)
+        state_dict3 = opt1['optimizer']
+    new_dict = state_dict1
+    for key,value in state_dict1.items():
+        print(key)
+        new_dict[key] = (state_dict1[key] + state_dict2[key] + state_dict3[key])/3
+    new_state['optimizer'] = new_dict
+    save_path = os.path.join(args.save_dir,'new_gen.pth')
+    torch.save(new_state,save_path)
+
+
+
