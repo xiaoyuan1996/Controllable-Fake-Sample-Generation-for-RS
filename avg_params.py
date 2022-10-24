5ad61c440340f5bd53835a8e88db459c63c72888
niuwh <niuwh@aircas.cn.com>
2022-10-24 13:02:41 +0800

    update12

--- avg_params.py
+++ avg_params.py
@@ -3,10 +3,10 @@ import argparse
 import os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-path1', '--path1', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1000000_E2124_gen.pth")
-    parser.add_argument('-path2', '--path2', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1010000_E2145_gen.pth")
-    parser.add_argument('-path3', '--path3', type=str, default="/data/diffusion_data/experiments/large_scale_384_220929_020243/checkpoint/I1020000_E2166_gen.pth")
-    parser.add_argument('-save_dir', '--save_dir', type=str, default="/data/diffusion_data/save_data/large_scale")
+    parser.add_argument('-path1', '--path1', type=str, default="/data/new_data/diffusion_data/experiments/crop_512_256_220905_015518/checkpoint/I1000000_E2834_gen.pth")
+    parser.add_argument('-path2', '--path2', type=str, default="/data/new_data/diffusion_data/experiments/crop_512_256_220905_015518/checkpoint/I990000_E2805_gen.pth")
+    parser.add_argument('-path3', '--path3', type=str, default="/data/new_data/diffusion_data/experiments/crop_512_256_220905_015518/checkpoint/I980000_E2777_gen.pth")
+    parser.add_argument('-save_dir', '--save_dir', type=str, default="/data/diffusion_data/save_data/crop_512_new")
     args = parser.parse_args()
     new_state ={'epoch': 1, 'iter': 1000000,
                      'scheduler': None, 'optimizer': None}
