4ab9abb7c9d2c90cf86111a71d84f6d29ed2861f
chongyang.hao <chongyang.hao@huarui.ai>
2022-12-28 10:56:37 +0800

    first submit

--- function_test.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import core.metrics as Metrics
-path = "/data/diffusion_data/infer/infer_128_220901_030446/results/hr_save/0_100_hr.png"
-img = Image.open(path).convert('RGB')
-# mean = np.mean(split_scores)
-mean  = Metrics.calculate_IS(img)
-#print(len(split_scores))
-print('IS is %.4f' % mean)
